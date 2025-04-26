313064e99ea5a77a6ce7dc292ce266b68547fe54
Dale Ren <daleren.y@gmail.com>
2025-04-26 00:59:15 -0500

    yo

--- daleyren-website/src/views/HomeView.vue
+++ daleyren-website/src/views/HomeView.vue
@@ -13,16 +13,15 @@ import NavHeader from "../components/NavHeader.vue";
         <template #content>
           <div>
             <div class="text-white sm:text-lg mt-6 md:ml-6 font-rubik text-base">
-              I'm a builder and student @ UChicago studying CS +
-              physics: trying to explore and learn as much as possible before I
+              I'm a builder and student @ UChicago studying cs +
+              stats: trying to explore and learn as much as possible before I
               get pulled into the abyss of software.<br />
               <br />
-              Currently, I'm a SWE intern at Shedd Capital working fullstack and a
-              student venture scout for Soma Capital.<br />
+              Currently, I'm a student venture scout for Soma Capital. Previously, I was a a SWE
+              intern at Shedd Capital working fullstack, small business
+              consultant intern at the Polsky Center, frontend software engineer intern at Haylon
+              Technologies, and research intern dabbling in cavity quantum electrodynamics.<br />
               <br />
-              Previously, I was a small business consultant intern at the Polsky
-              Center, frontend software engineer intern at Haylon Technologies, and
-              research intern dabbling in cavity quantum electrodynamics.
             </div>
           </div>
         </template>
